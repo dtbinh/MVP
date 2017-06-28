c2b0643e66bacb3d670610496cf7bbba0e3d2a57
Julia <Julia@10.16.116.125>
2017-06-28 14:57:20 -0400

    In progress

--- fcnFORCETRIM.m
+++ fcnFORCETRIM.m
@@ -1,44 +1,48 @@
 function [ rotorTHRUST, rotorAngINFLOW, rotorVelINFLOW,...
             rotorRPM, rotorPx, rotorPy, rotorMx,...
             dragBODYinduced, liftBODY, pitchVEHICLE] ...
-                = fcnFORCETRIM( V, rho, numrotors, geomBODYradius, dragVEHICLE, massVEHICLE )
+                = fcnFORCETRIM( flowq, flowRHO, numrotors, geomBODYradius, dragVEHICLE, massVEHICLE, vecROTORTABLE, vecANGLELIST )
 %% FORCE TRIM
 % This function determines the thrust and pitch attitude required to
 % maintain the multirotor vehicle in force equilibrium. 
 
     valWEIGHT           = massVEHICLE*9.81;                  % Vehicle weight - value
         
-    tempPITCHrad         = atan(dragVEHICLE/(valWEIGHT)); % First guess pitch - value
+    tempPITCHdeg         = atand(dragVEHICLE/(valWEIGHT)); % First guess pitch - value
 
     coefLIFT            = 1.8*(valPITCHrad); % Assume body has puck shape
     coefDRAGinduced     = 0.81*(valPITCHrad)^2;
     
-    liftBODY            = 0.5*rho*V^2*pi()*geomBODYradius^2*coefLIFT;
-    dragBODYinduced     = 0.5*rho*V^2*pi()*geomBODYradius^2*coefDRAGinduced;
+    liftBODY            = 0.5*flowRHO*V^2*pi()*geomBODYradius^2*coefLIFT;
+    dragBODYinduced     = 0.5*flowRHO*V^2*pi()*geomBODYradius^2*coefDRAGinduced;
     
     tempTHRUST          = sqrt((weightVEHICLE+liftBODY)^2 + ...
                                 (dragVEHICLE+dragBODYinduced)^2)/numrotors; % first guess thrust
     
-    tempTHRUSTrho       = tempTHRUST/rho;
+    tempTHRUSTrho       = tempTHRUST/flowRHO;
     
   %% look up rpm and Px based on thrust and inflow q and pitch
-    % temp outputs
-    rotorPx = 0;
-    rpm = 2000;
-    CP = 1;
-    CM = 1;
-    Py = 0;
-    Mx = 1;
-    My = 1;
-    Jint = 1;
+    % temp outputs for single rotors - to be saved into 3d matrices for
+    % variable 
+    % inputs: flowq, flowRHO, tempPITCHdeg, tempTHRUST, vecROTORTABLE, vecANGLELIST
+    
+    tempRPM = 2000;
+    tempPx = 0;
+    tempPy = 0;
+    tempMx = 1;
+    tempMy = 1;
+    tempCP = 1;
+    tempCMx = 1;
+    tempJinf = 1;
     
     %% inclues hover case
     
-    valPITCHrad = asin((dragVEHICLE/numrotors*tempTHRUST+Px*valWEIGHT/numrotors+...
-                                Px*liftBODY/numrotors+tempTHRUST*dragBODYinduced/numrotors)...
-                                /(tempTHRUST^2+Px^2));
+    valPITCHdeg = asind((dragVEHICLE/numrotors*tempTHRUST+tempPx*valWEIGHT/numrotors+...
+                                tempPx*liftBODY/numrotors+tempTHRUST*dragBODYinduced/numrotors)...
+                                /(tempTHRUST^2+tempPx^2));
     
-    tempTHRUST = sqrt((valWEIGHT/numrotors+liftBODY/numrotors+Px*sind(alpha2))^2 + (Dpar_tot/n+Di_body/n+Px*cosd(alpha2))^2);
+                             
+    tempTHRUST = sqrt((valWEIGHT/numrotors+liftBODY/numrotors+tempPx*sind(valPITCHdeg))^2 + (dragVEHICLE/numrotors+dragBODYinduced/numrotors+tempPx*cosd(valPITCHdeg))^2);
     rotorTHRUST     = repmat(tempTHRUST, 1, 1, geomNumROTORS);
 
         
